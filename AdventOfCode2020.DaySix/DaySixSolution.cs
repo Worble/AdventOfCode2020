0b0ca7220ed23cf2703680ce3aadb7e300b7a60e
worble <rsc93@pm.me>
2020-12-06 13:51:49 +0000

    day 6

---
+++ AdventOfCode2020.DaySix/DaySixSolution.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2020.DaySix
+{
+	public class DaySixSolution
+	{
+		private readonly List<GroupAnswers> _questionsAnsweredYes;
+
+		public DaySixSolution(string input)
+		{
+			var inputSplit = input.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
+			var current = new GroupAnswers();
+			_questionsAnsweredYes = new List<GroupAnswers>();
+			foreach (var answers in inputSplit)
+			{
+				answers.Trim();
+				if (string.IsNullOrWhiteSpace(answers))
+				{
+					if (current.Answers.Count > 0)
+					{
+						_questionsAnsweredYes.Add(current);
+						current = new GroupAnswers();
+					}
+					continue;
+				}
+				current.PeopleInGroup++;
+				foreach (var character in answers)
+				{
+					if (current.Answers.ContainsKey(character))
+					{
+						current.Answers[character]++;
+						continue;
+					}
+					current.Answers.Add(character, 1);
+				}
+			}
+			if (current.Answers.Count > 0)
+			{
+				_questionsAnsweredYes.Add(current);
+			}
+		}
+
+		public int SumGroupYes()
+		{
+			var total = 0;
+			foreach (var item in _questionsAnsweredYes)
+			{
+				var filtered = item.Answers.Where(e => e.Value == item.PeopleInGroup);
+				total += filtered.Count();
+			}
+			return total;
+		}
+
+		public int SumIndividualYes()
+		{
+			return _questionsAnsweredYes.Select(e => e.Answers.Count).Sum();
+		}
+	}
+
+	public class GroupAnswers
+	{
+		public Dictionary<char, int> Answers { get; set; } = new Dictionary<char, int>();
+		public int PeopleInGroup { get; set; } = 0;
+	}
+}
